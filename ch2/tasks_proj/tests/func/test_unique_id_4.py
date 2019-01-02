8723d53e39aec429bdc164fbea8b3cba828dbc1b
Val Melev <val.melev99@gmail.com>
2019-01-02 13:11:47 +0100

    Demonstrate xfail & xpass

--- ch2/tasks_proj/tests/func/test_unique_id_4.py
+++ ch2/tasks_proj/tests/func/test_unique_id_4.py
@@ -15,7 +15,7 @@ def initialized_tasks_db(tmpdir):
     tasks.stop_tasks_db()
 
 
-@pytest.mark.skipif(
+@pytest.mark.xfail(
     tasks.__version__ < '0.2.0',
     reason='not supported until version 0.2.0'
 )
@@ -26,13 +26,15 @@ def test_unique_id_1():
     assert id_1 != id_2
 
 
-def test_unique_id_2():
-    """unique_id() should return an unused id."""
-    ids = []
-    ids.append(tasks.add(Task('one')))
-    ids.append(tasks.add(Task('two')))
-    ids.append(tasks.add(Task('three')))
-    # grab a unique id
-    uid = tasks.unique_id
-    # make sure it isn't in the list of existing ids
-    assert uid not in ids
+@pytest.mark.xfail()
+def test_unique_id_is_a_duck():
+    """Demonstrate xfail."""
+    uid = tasks.unique_id()
+    assert uid == 'a duck'
+
+
+@pytest.mark.xfail()
+def test_unique_id_is_not_a_duck():
+    """Demonstrate xpass."""
+    uid = tasks.unique_id()
+    assert uid != 'a duck'
